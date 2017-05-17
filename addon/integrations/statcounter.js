375600c1bb731a3019767b753488345d8e809920
Joren l'Ami <joren@distinct.nl>
2017-05-17 12:56:22 +0200

    beta2: trackEvents

--- addon/integrations/statcounter.js
+++ addon/integrations/statcounter.js
@@ -15,18 +15,18 @@ export default Base.extend({
 			
 		if (window.sc_clickstat_call) {
 			//let _29 = ....
-			let page_info = "&resolution=" + sc_width + "&h=" + sc_height + "&camefrom=" + escape(this.oldViewURI.substring(0, 600)) + "&u=" + escape(viewURI.substring(0, 600)) + "&t=" + sc_title + "&sc_snum=" + sc_script_num + sc_pageview_tag_string + "&sess=7a9eb4&p=0&invisible=1";
+			let page_info = "&resolution=" + sc_width + "&h=" + sc_height + "&camefrom=" + escape(window.sc_url.substring(0, 600)) + "&u=" + escape(viewURI.substring(0, 600)) + "&t=" + sc_title + "&sc_snum=" + sc_script_num + sc_pageview_tag_string + "&sess=7a9eb4&p=0&invisible=1";
 			let uri = sc_base_dir + "&sc_random=" + Math.random() + sc_unique_returning + page_info;
 			
 			let i = new Image();
 			i.src = uri;
 		}
 		
-		this.oldViewURI = viewURI;
+		window.sc_url = viewURI;
 	},
 
 	trackEvent(options = {}) {
-		//TODO: use sc_clickstat_call(uri, options)
+		sc_clickstat_call(options.uri, sc_click_dir);
 	},
 	
 	insertTag: Ember.on('init', function() {
